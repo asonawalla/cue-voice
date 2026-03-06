e3298d301622d1a989cbc01d7a1e8a739891ef15
Azim Sonawalla <azim.sonawalla@gmail.com>
2026-03-05 23:51:58 -0500

    Scaffold Cue Xcode project

---
+++ Cue/CueApp.swift
@@ -0,0 +1,17 @@
+//
+//  CueApp.swift
+//  Cue
+//
+//  Created by Azim Sonawalla on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct CueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CueTests/CueTests.swift
@@ -0,0 +1,17 @@
+//
+//  CueTests.swift
+//  CueTests
+//
+//  Created by Azim Sonawalla on 3/5/26.
+//
+
+import Testing
+@testable import Cue
+
+struct CueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
